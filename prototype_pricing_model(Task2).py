66fc00e92671fdee1ecec4c0d8efe9c8c0f8659e
Rudi3601 <rudragopal3601@gmail.com>
2024-07-07 22:01:42 -0400

    Changes to prototype_pricing_model

--- prototype_pricing_model(Task2).py
+++ prototype_pricing_model(Task2).py
@@ -4,3 +4,20 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from datetime import datetime, timedelta
+
+
+"""Loading data"""
+
+data = pd.read_csv('Nat_Gas.csv', parse_dates = ['Dates'])
+data['Prices'] = data['Prices'].astype(float)
+
+
+
+"""Initializing predictor and dependent values for Linear Regression"""
+
+X = data['Dates'].map(datetime.toordinal).values.reshape(-1, 1)
+y = data['Prices'].values
+
+last_date = data['Dates'].max()
+future_dates = []
+
