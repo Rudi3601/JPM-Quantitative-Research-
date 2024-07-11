d103a97fe673cdd8ea4098b18f73c8107eb8fd9b
Rudi3601 <rudragopal3601@gmail.com>
2024-07-10 21:31:40 -0400

    Update prototype_pricing_model(Task2).py

--- prototype_pricing_model(Task2).py
+++ prototype_pricing_model(Task2).py
@@ -21,3 +21,19 @@ y = data['Prices'].values
 last_date = data['Dates'].max()
 future_dates = []
 
+for i in range(1, 13):
+    future_date = last_date + timedelta(days=30*i)
+    future_dates.append(future_date)
+
+
+future_dates_ordinal = np.array([date.tooridnal() for date in future_dates]).reshape(-1, 1)
+
+future_prices = LinearRegression().fit(X, y).predict(future_dates_ordinal)
+
+def future_day_price(day_str):
+    day = datetime.strptime(day_str, '%Y-%m-%d')
+    day_ordinal = day.toordinal()
+
+    return future_prices[day_ordinal]
+    
+
